3fa397146fdddcc51a37e7061563f73f4a718c13
Luis <garcia.luis.enrique19@gmail.com>
2023-06-25 16:05:26 -0700

    Setting up the table

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,12 @@
 import { useState } from 'react'
 import './App.css'
+import ContactList from './components/ContactList';
 
-function App() {
- 
 
+export default function App() {
   return (
     <>
-
+      <ContactList />
     </>
-  )
+  );
 }
-
-export default App
